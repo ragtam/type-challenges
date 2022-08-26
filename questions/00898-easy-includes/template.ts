f9987d116887fc477747463f85ac55ff1b422485
ragtam <mag190033@gmail.com>
2022-08-26 09:04:37 +0200

    includes

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,8 @@
-type Includes<T extends readonly any[], U> = any
+type Equal<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? true : false
+
+type Includes<T extends unknown[], U> = T extends [infer First, ...infer Rest] ? Equal<U, First> extends true ? true : Includes<Rest, U> : false
+
+// https://www.youtube.com/watch?v=ipwWXQQFTPU
+// naive implementation:
+// type Includes<T extends unknown[], U> = U extends T[number] ? true : false
