e03b145f8f60f6d0f9ee3215ed33bf4b13a17e7e
ragtam <mateusz.garbaciak1@gmail.com>
2022-10-21 16:52:02 +0200

    medium-omit

--- questions/00003-medium-omit/template.ts
+++ questions/00003-medium-omit/template.ts
@@ -1,1 +1,2 @@
-type MyOmit<T, K> = any
+type RemainingType<TType, KProp> = TType extends KProp ? never : TType
+type MyOmit<TItem, KProp extends keyof TItem> = { [ Property in RemainingType<keyof TItem, KProp> ]: TItem[Property] }
