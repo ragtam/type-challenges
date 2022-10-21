841ea05820815c84c2f4086e84c70b2201da7ea0
ragtam <mateusz.garbaciak1@gmail.com>
2022-10-21 17:21:50 +0200

    medium-readonly2

--- questions/00008-medium-readonly-2/template.ts
+++ questions/00008-medium-readonly-2/template.ts
@@ -1,1 +1,6 @@
-type MyReadonly2<T, K> = any
+type PickAllBut<TItem, KProp extends keyof TItem> = Pick<TItem, KProp>
+type PickAll<TItem, KProp extends keyof TItem> = Omit<TItem, KProp>
+type MakeReadonly<TItem> = { readonly [Property in keyof TItem]: TItem[Property] }
+
+type MyReadonly2<TItem, KReadonlyProps extends keyof TItem = keyof TItem> =
+    PickAll<TItem, KReadonlyProps> & MakeReadonly<PickAllBut<TItem, KReadonlyProps>>
